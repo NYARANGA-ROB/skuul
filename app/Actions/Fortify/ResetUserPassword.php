1a16ca34bd6acb70e2801f6b8eb67833d95ef803
Robert Nyaranga <101572972+NYARANGA-ROB@users.noreply.github.com>
2026-04-08 08:07:45 +0300

    Update ResetUserPassword.php

--- app/Actions/Fortify/ResetUserPassword.php
+++ app/Actions/Fortify/ResetUserPassword.php
@@ -8,7 +8,6 @@ use Laravel\Fortify\Contracts\ResetsUserPasswords;
 class ResetUserPassword implements ResetsUserPasswords
 {
     use PasswordValidationRules;
-
     /**
      * Validate and reset the user's forgotten password.
      *
