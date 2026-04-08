f22ffd16ab29a29ee9e739a6f67e84c2ec48f374
Robert Nyaranga <101572972+NYARANGA-ROB@users.noreply.github.com>
2026-04-08 08:09:19 +0300

    Update UpdateUserPassword.php

--- app/Actions/Fortify/UpdateUserPassword.php
+++ app/Actions/Fortify/UpdateUserPassword.php
@@ -3,7 +3,6 @@
 namespace App\Actions\Fortify;
 
 use Illuminate\Support\Facades\Hash;
-
 use Illuminate\Support\Facades\Validator;
 use Laravel\Fortify\Contracts\UpdatesUserPasswords;
 class UpdateUserPassword implements UpdatesUserPasswords
