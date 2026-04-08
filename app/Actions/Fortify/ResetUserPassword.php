5282d5f051cfe3282c0e6219ba53129c43951b24
Robert Nyaranga <101572972+NYARANGA-ROB@users.noreply.github.com>
2026-04-08 08:06:57 +0300

    Update ResetUserPassword.php

--- app/Actions/Fortify/ResetUserPassword.php
+++ app/Actions/Fortify/ResetUserPassword.php
@@ -5,6 +5,7 @@ namespace App\Actions\Fortify;
 use Illuminate\Support\Facades\Hash;
 
 use Illuminate\Support\Facades\Validator;
+
 use Laravel\Fortify\Contracts\ResetsUserPasswords;
 
 class ResetUserPassword implements ResetsUserPasswords
