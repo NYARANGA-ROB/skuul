7d8eed0eea2464515f1e9de01b8c460a1a318b06
Robert Nyaranga <101572972+NYARANGA-ROB@users.noreply.github.com>
2026-04-15 08:35:59 +0300

    Update Money.php

--- app/Casts/Money.php
+++ app/Casts/Money.php
@@ -3,7 +3,6 @@ namespace App\Casts;
 use Brick\Money\Money as BrickMoney;
 use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
 use Illuminate\Database\Eloquent\Model;
-
 class Money implements CastsAttributes
 {
     /**
