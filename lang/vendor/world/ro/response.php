13977a57512f3bf9ad2419a0fb5ae8b06ca64053
Robert Nyaranga <101572972+NYARANGA-ROB@users.noreply.github.com>
2026-04-16 08:32:36 +0300

    Update response.php

--- lang/vendor/world/ro/response.php
+++ lang/vendor/world/ro/response.php
@@ -13,6 +13,7 @@ return [
 		'city' => 'oraș|orase',
     
 		'state' => 'judet|judete',
+    
 		'timezone' => 'fus orar|fusuri orare',
 		'currency' => 'valută|valute',
 		'language' => 'limba|limbi',
