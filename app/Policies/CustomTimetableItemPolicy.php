308b7620fb23b13b8f8ab9d3149b7392b5e92f2f
Robert Nyaranga <101572972+NYARANGA-ROB@users.noreply.github.com>
2026-04-23 22:06:50 +0300

    Update CustomTimetableItemPolicy.php

--- app/Policies/CustomTimetableItemPolicy.php
+++ app/Policies/CustomTimetableItemPolicy.php
@@ -4,7 +4,6 @@ namespace App\Policies;
 use App\Models\CustomTimetableItem;
 use App\Models\User;
 use Illuminate\Auth\Access\HandlesAuthorization;
-
 class CustomTimetableItemPolicy
 {
     use HandlesAuthorization;
