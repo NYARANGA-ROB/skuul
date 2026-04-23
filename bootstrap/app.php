a92212420b5476b75cb0b74ba41bb47a54ab3d73
Robert Nyaranga <101572972+NYARANGA-ROB@users.noreply.github.com>
2026-04-23 21:41:45 +0300

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -30,7 +30,6 @@ $app->singleton(
     Illuminate\Contracts\Console\Kernel::class,
     App\Console\Kernel::class
 );
-
 $app->singleton(
     Illuminate\Contracts\Debug\ExceptionHandler::class,
     App\Exceptions\Handler::class
