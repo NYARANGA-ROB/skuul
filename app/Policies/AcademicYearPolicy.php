2eec4d29bbe60a5c8b4b9664f758732dbe4fc543
Robert Nyaranga <101572972+NYARANGA-ROB@users.noreply.github.com>
2026-04-23 21:57:27 +0300

    Remove unnecessary whitespace in AcademicYearPolicy

--- app/Policies/AcademicYearPolicy.php
+++ app/Policies/AcademicYearPolicy.php
@@ -4,7 +4,6 @@ namespace App\Policies;
 use App\Models\AcademicYear;
 use App\Models\User;
 use Illuminate\Auth\Access\HandlesAuthorization;
-
 class AcademicYearPolicy
 {
     use HandlesAuthorization;
